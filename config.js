833d48ec2a66b8e8576eda2f81e2044878942144
alecmanabat <alecmanabat1@gmail.com>
2017-06-19 20:11:40 -0400

    add stuff

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    stateFile: "./appstate.json",
+    devID: "100000478365455",
+    dictionaryID: "fd17d18d",
+    dictionaryKey: "0f46457af0b50a5688b646fed31e4c75",
+    geocoderKey: "AIzaSyCYvQCm7o0-3YXEQ5yC5ce3vAW1oHxNwH0",
+    mongoURI: "mongodb://alecmanabat1:Bishop2020!!@alec1-shard-00-00-pqdl1.mongodb.net:27017,alec1-shard-00-01-pqdl1.mongodb.net:27017,alec1-shard-00-02-pqdl1.mongodb.net:27017/<DATABASE>?ssl=true&replicaSet=Alec1-shard-0&authSource=admin",
+    newsKey: "a4bc950380944b1b9cafd2ff6fadfd96",
+    spotifyKey: "5ec7157f071c4e37b3ee74e098e66a13:11346c053b40419a8c06d904db9ec6a6",
+    weatherKey: "777dc1f69704de6280920a00ebcd150e",
+    wolframKey: "KQKTGE-6HUP4EE585",
+    youtubeKey: "AIzaSyBraUZ-Z_Zt_PZjK8_UUzIqfAU5YbgPbQ0"
+};
